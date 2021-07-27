c85e6302a8035517e0e891164028e38fa593f0cb
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2021-07-27 11:33:33 +0200

    Improve setup tools

--- setup.py
+++ setup.py
@@ -2,9 +2,25 @@
 
 from distutils.core import setup
 
-setup(name='Dradis APi',
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(name='dradis-api',
       version='1.0',
       description='Dradis API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Northwave B.V.',
+      url="https://github.com/NorthwaveSecurity/dradis-api",
+      license='LGPL',
       py_modules=['dradis'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: LGPL License',
+
+          'Programming Language :: Python :: 3',
+      ],
+      keywords='dradis api',
+      python_requires='>=3',
       )

2b545977d0ac5efa2b978087b267521407716d17
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2021-10-07 14:39:51 +0200

    Update te version 1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
     long_description = f.read()
 
 setup(name='dradis-api',
-      version='1.0',
+      version='1.1',
       description='Dradis API',
       long_description=long_description,
       long_description_content_type="text/markdown",

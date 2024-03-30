f0c6470e3dd36db35e86dec20f67ff9bf7df0bcc
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2024-03-30 21:51:54 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md",encoding="utf-8") as f:
     long_description = f.read()
 
 setup(name='dradis-api',
-      version='1.1',
+      version='1.2',
       description='Dradis API',
       long_description=long_description,
       long_description_content_type="text/markdown",
